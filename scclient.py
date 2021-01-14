e8dcff20295816d0b24db848e0f00e0f1f262d79
gentmantan <gentmantan@gmail.com>
2021-01-14 18:43:00 -0500

    Added text box

--- scclient.py
+++ scclient.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.scrolledtext as st
 
 root = Tk()
 
@@ -6,25 +7,23 @@ root = Tk()
 def myClick():
     myLabel1 = Label(root, text="")
     myLabel1 = Label(root, text=myEntry.get())
-    myLabel1.grid(row=0, column=0)
+    myLabel1.grid(row=2, column=0)
 
 root.geometry("800x800")
 
 #Create scrolled text widget
-text_area = ScrolledText(win, wrap=tk.WORD, width=40, height=10, font=("Times New Roman"))
+text_area = st.ScrolledText(width=40, height=10)
 
 #Create a label widget
-myLabel1 = Label(root)
+#myLabel1 = Label(root)
 myButton = Button(root, text="Enter", command=myClick)
 myEntry = Entry(root)
 
-
-
-
 #Orient widgets
-myLabel1.grid(row=0, column=0)
+#myLabel1.grid(row=0, column=0)
 myButton.grid(row=1, column=1)
 myEntry.grid(row=1, column=0)
+text_area.grid(row=0, column=0)
 
 #Run window
 root.mainloop()
