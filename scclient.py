fb30f0e81e412e163a981736f15005335b3439ce
gentmantan <gentmantan@gmail.com>
2021-01-15 15:57:28 -0500

    Reoriented UI added second console

--- scclient.py
+++ scclient.py
@@ -9,21 +9,34 @@ def myClick():
     myLabel1 = Label(root, text=myEntry.get())
     myLabel1.grid(row=2, column=0)
 
-root.geometry("800x800")
+#Create frames
+frame0 = LabelFrame(root) 
+frame1 = LabelFrame(frame0, text="Main Console", padx=10, pady=10) 
+frame2 = LabelFrame(frame0, text="Status", padx=10, pady=10) 
 
-#Create scrolled text widget
-text_area = st.ScrolledText(width=40, height=10)
+#Create scrolled text widgets
+text_area1 = st.ScrolledText(frame1, width=40, height=10)
+text_area1.insert(INSERT, "SHOW ME WHAT YOU GOT CAPTAIN") 
+text_area1.configure(state='disabled')
+text_area2 = st.ScrolledText(frame2, width=40, height=10)
+text_area2.insert(INSERT, "SHIP'S FINE") 
+text_area2.configure(state='disabled')
 
-#Create a label widget
-#myLabel1 = Label(root)
+
+
+#Create other widgets
 myButton = Button(root, text="Enter", command=myClick)
-myEntry = Entry(root)
+myEntry = Entry(root, width=50)
 
 #Orient widgets
-#myLabel1.grid(row=0, column=0)
+#myButton.grid(row=1, column=1)
+frame0.grid(row=0, column=0, columnspan=2)
+frame1.grid(row=0, column=0)
+frame2.grid(row=0, column=1)
 myButton.grid(row=1, column=1)
 myEntry.grid(row=1, column=0)
-text_area.grid(row=0, column=0)
+text_area1.pack()
+text_area2.pack()
 
 #Run window
 root.mainloop()
